c09e913527ffdb767cde429f07a4bca43ba1527d
securitycube <vishalguptaccube@gmail.com>
2025-07-15 20:37:23 +1000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,83 @@
+(function ($) {
+  "use strict";
+
+  $(document).ready(function () {
+
+    var swiper = new Swiper(".testimonial-swiper", {
+      slidesPerView: 1,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+
+    // product single page
+    var thumb_slider = new Swiper(".product-thumbnail-slider", {
+      autoplay: true,
+      loop: true,
+      spaceBetween: 8,
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesProgress: true,
+    });
+
+    var large_slider = new Swiper(".product-large-slider", {
+      autoplay: true,
+      loop: true,
+      spaceBetween: 10,
+      effect: 'fade',
+      thumbs: {
+        swiper: thumb_slider,
+      },
+    });
+
+    $('.navbar').on('click', '.search-toggle', function (e) {
+      var selector = $(this).data('selector');
+
+      $(selector).toggleClass('show').find('.search-input').focus();
+      $(this).toggleClass('active');
+
+      e.preventDefault();
+    });
+
+    window.addEventListener("load", (event) => {
+      //isotope
+      $('.isotope-container').isotope({
+        // options
+        itemSelector: '.item',
+        layoutMode: 'masonry',
+      });
+
+      // Initialize Isotope
+      var $container = $('.isotope-container').isotope({
+        // options
+        itemSelector: '.item',
+        layoutMode: 'masonry',
+      });
+
+      $(document).ready(function () {
+        //active button
+        $('.filter-button').click(function () {
+          $('.filter-button').removeClass('active');
+          $(this).addClass('active');
+        });
+      });
+
+      // Filter items on button click
+      $('.filter-button').click(function () {
+        var filterValue = $(this).attr('data-filter');
+        if (filterValue === '*') {
+          // Show all items
+          $container.isotope({ filter: '*' });
+        } else {
+          // Show filtered items
+          $container.isotope({ filter: filterValue });
+        }
+      });
+
+    });
+
+  }); // End of a document
+
+
+})(jQuery);
